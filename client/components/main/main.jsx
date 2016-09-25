e8fe619cbf775f32ffba04b42d220a164c73b979
Jan Crisologo <rtql8d@gmail.com>
2016-09-24 23:27:21 -0600

    Styled navbar menu

---
+++ client/components/main/main.jsx
@@ -0,0 +1,15 @@
+Main = React.createClass({
+    render(){
+        return (
+            <div className="col-sm-9 col-md-11" id="main">
+                <div className="full col-sm-9">
+                    <div className="row">
+                        <div className="col-sm-9">
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+});
