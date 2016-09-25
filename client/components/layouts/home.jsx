a32857c9ac84466661af114a811ed50fcbf741ff
Jan Crisologo <rtql8d@gmail.com>
2016-09-24 21:24:36 -0600

    Set up routes

---
+++ client/components/layouts/home.jsx
@@ -0,0 +1,19 @@
+Homelayout = React.createClass({
+    render(){
+        return (
+            <div>
+                Header
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-6">
+                            Features
+                        </div>
+                        <div className="col-md-5 col-md-offset-1">
+                            Signup
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+});
