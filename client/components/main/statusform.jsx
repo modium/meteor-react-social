783644e19b57edf4cd12af7294ae27a7ab285342
Jan Crisologo <rtql8d@gmail.com>
2016-09-25 22:50:11 -0600

    Handled post submission

--- client/components/main/statusform.jsx
+++ client/components/main/statusform.jsx
@@ -5,7 +5,36 @@ Statusform = React.createClass({
             filename:''
         }
     },
-
+    resetFields(){
+        ReactDOM.findDOMNode(this.refs.sharing).value = '';
+        ReactDOM.findDOMNode(this.refs.imageid).value = '';
+        ReactDOM.findDOMNode(this.refs.sharing).focus();
+    },
+    submitForm(e){
+        e.preventDefault();
+        var message = this.refs.sharing.value; //get the value of ref="sharing"
+        var imageid = this.refs.imageid.value;
+        if(imageid){
+            var image = Images.findOne({_id:imageid});
+            var imageurl = image.url();
+        }
+        Meteor.call('Posts.insert',message,imageid,imageurl,function(err){
+            if(err){
+                console.log(err);
+            }
+        });
+        this.setState({image:'',filename:''}); //reset image and filename
+        this.resetFields();
+    },
+    uploadFile(e){
+        e.preventDefault();
+        var that = this;
+        FS.Utility.eachFile(e,function(file){
+            Images.insert(file,function(err,fileObj){
+                that.setState({image:fileObj._id,filename:fileObj.data.blob.name}); //loop through if user uploads multiple files
+            });
+        });
+    },
     render(){
         return (
             <div className="panel panel-default">
@@ -13,7 +42,7 @@ Statusform = React.createClass({
                     <div className="panel-heading">
                         Update Status
                     </div>
-                    <form className="form center-block">
+                    <form onSubmit={this.submitForm} className="form center-block">
                         <input type="hidden" ref="imageid" value={this.state.image}/>
                         <div className="panel-body">
                             <div className="form-group">
@@ -25,7 +54,7 @@ Statusform = React.createClass({
                             <div className="panel-footer">
                                 <div>
                                     <ul className="pull-left list-inline">
-                                        <li><input ref="file" type="file" className="filepicker" id="file"/></li>
+                                        <li><input onChange={this.uploadFile} ref="file" type="file" className="filepicker" id="file"/></li>
                                         <button className="btn btn-primary btn-sm postbutton">Post</button>
                                     </ul>
                                 </div>
