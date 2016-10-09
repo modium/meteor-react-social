64479883d05d798d5b56d9c7786437b64252842d
Jan Crisologo <rtql8d@gmail.com>
2016-10-08 19:08:11 -0600

    Implemented likes functionality

--- client/components/post/post.jsx
+++ client/components/post/post.jsx
@@ -1,4 +1,34 @@
 Post = React.createClass({
+    mixins:[ReactMeteorData],
+    getMeteorData(){
+        return {
+            currentUser:Meteor.userId()
+        }
+    },
+    likePost(e){
+        e.preventDefault();
+        var postid = this.props.post._id;
+        Meteor.call('likePost',this.data.currentUser,postid); //Send current user's ID and post's ID to server function
+    },
+    renderLikes(){
+        var likes = '';
+        var likesub = 0;
+        var currentUser = String(Meteor.userId());
+        if(this.props.post.likes.indexOf(currentUser) !== -1){
+            likes = 'You and ';
+            likesub = 1;
+        }
+        switch (this.props.post.likes.length - likesub) {
+            case 0:
+                return likesub > 0 ? 'You like this':'';
+            case 1:
+                return likesub > 0 ? likes + '1 other person like this':'1 person likes this';
+                break;
+            default:
+                return likes + (this.props.post.likes.length - likesub) + ' people like this';
+                break;
+        }
+    },
     render(){
         var dimage = '';
 
@@ -18,9 +48,8 @@ Post = React.createClass({
                 <div className="panel panel-white post panel-shadow">
                     <div className="post-heading">
                         <div className="pull-left image">
-                            {/*Avatar*/}
                             <Avatar klass="img-circle avatar" user={this.props.post.user._id}/>
-                            <img src="http://placehold.it/48x48" alt="" className="img-circle"/>
+                            {/*<img src="http://placehold.it/48x48" alt="" className="img-circle"/>*/}
                         </div>
                         <div className="pull-left meta">
                             <div className="title h5">
@@ -40,10 +69,10 @@ Post = React.createClass({
                         <br/>
                     </div>
                     <div className="actions">
-                        <a href="#" className="btn btn-default stat-item">
+                        <a onClick={this.likePost} href="#" className="btn btn-default stat-item">
                             <i className="fa fa-thumbs-up icon"></i>
                         </a>&nbsp;
-                        10 Likes
+                        {this.renderLikes(this.props.post.likes.length)}
                         <div className="post-footer">
                             Comments List
                         </div>
